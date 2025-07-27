ece95ea505d76163f716715f2763641ffa773ba9
vrnkxs <kuliba.veronika@gmail.com>
2025-07-26 20:24:26 -0400

    Add files via upload

---
+++ AutomationProjectB5/src/test/java/io/loop/test/day2/SeleniumTest.java
@@ -0,0 +1,11 @@
+package io.loop.test.day2;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumTest {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.loopcamp.io");
+    }
+}
