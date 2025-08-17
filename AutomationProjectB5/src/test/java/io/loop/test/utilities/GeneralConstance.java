4fc64cb1d70ebf5335e519354ea5de30a05daa8d
vrnkxs <kuliba.veronika@gmail.com>
2025-08-17 09:39:09 -0400

    day5

--- AutomationProjectB5/src/test/java/io/loop/test/utilities/GeneralConstance.java
+++ AutomationProjectB5/src/test/java/io/loop/test/utilities/GeneralConstance.java
@@ -7,5 +7,6 @@ public class GeneralConstance {
     public static final String FIREFOX = "firefox";
     public static final String SAFARI = "safari";
 
-
+    //herokuapp url
+    public static final String HEROKUAPP_URL = "https://the-internet.herokuapp.com/forgot_password";
 }
--- AutomationProjectB5/src/test/java/io/loop/test/utilities/LoopCampConstance.java
+++ AutomationProjectB5/src/test/java/io/loop/test/utilities/LoopCampConstance.java
@@ -9,5 +9,5 @@ public class LoopCampConstance {
     public static final String EXPECTED_HEADER_FOR_FORM = "Registration form";
     public static final String EXPECTED_PLACEHOLDER_FOR_FIRSTNAME = "first name";
 
-
+    public static final String RADIOBUTTON_URL = "https://loopcamp.vercel.app/radio-buttons.html";
 }
